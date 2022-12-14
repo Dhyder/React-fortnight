766607401684d1f808420dee2762bffcfab70b0b
VickyShiraa <desastrecaliente@gmail.com>
2022-12-14 20:20:30 +0300

    added and styled Feature and possibity components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -33,7 +33,7 @@ const Navbar = () => {
         </div>
         <div className="fortnight__navbar-links_container">
           <p><a href="#home">Home</a></p>
-          <p><a href="#fortnight">Fortnight</a></p>
+          <p><a href="#f3">Fortnight</a></p>
           <p><a href="#possibility">Open AI</a></p>
           <p><a href="#features">Case Studies</a></p>
           <p><a href="#blog">Library</a></p>
