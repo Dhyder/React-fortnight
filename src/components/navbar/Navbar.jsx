b7f56e5a30a9f9a9de858d39e5affdcc04076c41
VickyShiraa <desastrecaliente@gmail.com>
2022-12-14 17:01:52 +0300

    add and styled header and navbar components

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,15 +1,61 @@
 import React from 'react';
+import { useState } from 'react';
 import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
 import logo from '../../assets/logo.svg'
 import './navbar.css';
 
+const Menu = () => (
+  <>
+  <p><a href="#home">Home</a></p>
+  <p><a href="#fortnight">Fortnight</a></p>
+  <p><a href="#possibility">Open AI</a></p>
+  <p><a href="#features">Case Studies</a></p>
+  <p><a href="#blog">Library</a></p>
+  </>
+)
+
+const Authmobile = () => (
+  <>
+  <div className="fortnight__navbar-menu_container-links-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+  </div>
+  </>
+)
+
 const Navbar = () => {
+  const [toggleMenu, setToggleMenu] = useState(false)
   return (
     <div className='fortnight__navbar'>
       <div className="fortnight__navbar-links">
         <div className="fortnight__navbar-links_logo">
           <img src={logo} alt="logo" />
         </div>
+        <div className="fortnight__navbar-links_container">
+          <p><a href="#home">Home</a></p>
+          <p><a href="#fortnight">Fortnight</a></p>
+          <p><a href="#possibility">Open AI</a></p>
+          <p><a href="#features">Case Studies</a></p>
+          <p><a href="#blog">Library</a></p>
+        </div>
+      </div>
+      <div className="fortnight__navbar-sign">
+        <p>Sign In</p>
+        <button type="button">Sign Up</button>
+      </div>
+      <div className="fortnight__navbar-menu">
+        {toggleMenu
+        ?<RiCloseLine color="white" size={27} onClick={() => setToggleMenu(false)} />
+        :<RiMenu3Line color="white" size={27} onClick={() => setToggleMenu(true)} />
+        }
+        {toggleMenu && (
+          <div className="fortnight__navbar-menu_container scale-up-center">
+            <div className="fortnight__navbar-menu_container-links">
+              <Menu/>
+              <Authmobile/>
+            </div>
+          </div>
+        )}
       </div>
     </div>
   )
--- src/containers/header/Header.jsx
+++ src/containers/header/Header.jsx
@@ -1,9 +1,31 @@
 import React from 'react';
 import './header.css';
+import people from '../../assets/people.png'
+import ai from '../../assets/ai.png'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div className="fortnight__header section__padding" id='home'>
+      <div className="fortnight__header-content">
+        <h1 className="gradient__text">
+          Discovering Something Amazing With Fortnight with Open AI
+        </h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ipsum amet earum, illo debitis vitae pariatur minima cumque commodi saepe! Vitae voluptas, eaque ducimus pariatur assumenda nostrum suscipit. Ab, aut?
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Ex ipsum amet earum, illo debitis vitae pariatur minima cumque commodi saepe! Vitae voluptas, eaque ducimus pariatur assumenda nostrum suscipit. Ab, aut?
+        </p>
+        <div className="fortnight__header-content__input">
+          <input type="email" name="" id="" placeholder='Your Email Address' required />
+          <button type="button">Get Started</button>
+        </div>
+        <div className="fortnight__header-content__people">
+          <img src={people} alt='people'/>
+          <p>1,600 people requested access in the last 24 hours</p>
+        </div>
+        <div className="fortnight__header-image">
+          <img src={ai} alt="ai" />
+        </div>
+      </div>
+    </div>
   )
 }
 
