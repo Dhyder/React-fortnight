dbdfac178d3d685379d9e44c2595347a90733eb7
VickyShiraa <desastrecaliente@gmail.com>
2022-12-14 21:12:59 +0300

    added and styled article, CTA and footer components

--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,9 +1,48 @@
 import React from 'react';
+import fortnightLogo from '../../assets/logo.svg';
 import './footer.css';
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <div className="fortnight__footer section__padding">
+    <div className="fortnight__footer-heading">
+      <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+    </div>
+
+    <div className="fortnight__footer-btn">
+      <p>Request Early Access</p>
+    </div>
+
+    <div className="fortnight__footer-links">
+      <div className="fortnight__footer-links_logo">
+        <img src={fortnightLogo} alt="fortnight_logo" />
+        <p>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+      </div>
+      <div className="fortnight__footer-links_div">
+        <h4>Links</h4>
+        <p>Overons</p>
+        <p>Social Media</p>
+        <p>Counters</p>
+        <p>Contact</p>
+      </div>
+      <div className="fortnight__footer-links_div">
+        <h4>Company</h4>
+        <p>Terms & Conditions </p>
+        <p>Privacy Policy</p>
+        <p>Contact</p>
+      </div>
+      <div className="fortnight__footer-links_div">
+        <h4>Get in touch</h4>
+        <p>Crechterwoord K12 182 DK Alknjkcb</p>
+        <p>085-132567</p>
+        <p>info@payme.net</p>
+      </div>
+    </div>
+
+    <div className="fortnight__footer-copyright">
+      <p>@2022 FortNight. All rights reserved.</p>
+    </div>
+  </div>
   )
 }
 
